712cb78284464e5913af81934fff18678ea28fd4
Ookiineko <chiisaineko@protonmail.com>
2024-07-14 19:20:23 +0900

    android: request storage permission on Android M and above
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- android/src/fluddokt/opsu/android/AndroidLauncher.java
+++ android/src/fluddokt/opsu/android/AndroidLauncher.java
@@ -1,5 +1,7 @@
 package fluddokt.opsu.android;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -16,6 +18,12 @@ public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+			&& checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+			requestPermissions(new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+			finish();
+			return;
+		}
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.useImmersiveMode = true;
 		config.useWakelock = true;
